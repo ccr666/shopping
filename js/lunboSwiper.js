8c4ea340883e84c239b148b8d5be7f4db3ae0359
ccr666 <1464296676@qq.com>
2020-02-22 12:57:51 +0800

    first commit

---
+++ js/lunboSwiper.js
@@ -0,0 +1,19 @@
+<script src="../plugin/js/jquery-1.11.3.js"></script>
+
+  // Initialize Swiper 
+ 
+    var swiper = new Swiper('.swiper-container', {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      slidesPerGroup: 3,
+      loop: true,
+      loopFillGroupWithBlank: true,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
